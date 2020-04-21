29db1d54e7f67c5ef278ee168bf4ed23ba4def35
YangHS <yanghs632@gmail.com>
2020-04-21 17:52:06 +0900

    Prototype Init Activity Set KeyEvent unusable

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,19 +1,44 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
+
 
 public class MainActivity extends AppCompatActivity {
-    public void onBackPressed() {
-        //super.onBackPressed();
+
+    //기능: 키이벤트 발생 시 처리
+    public boolean onKeyDown(int keycode, KeyEvent event){
+        if(checkKeyButton(keycode)){
+            return true;
+        }
+        return super.onKeyDown(keycode, event);
+    }
+
+    //기능: 눌린 키가 볼륨키 혹은 소프트키인지 식별 뒤 해당 키라면 T, 아니라면 F
+    protected boolean checkKeyButton(int keycode){
+        if(keycode == KeyEvent.KEYCODE_BACK ||
+                keycode == KeyEvent.KEYCODE_VOLUME_DOWN ||
+                keycode == KeyEvent.KEYCODE_VOLUME_UP ||
+                keycode == KeyEvent.KEYCODE_SOFT_LEFT ||
+                keycode == KeyEvent.KEYCODE_SOFT_RIGHT ){
+            return true;
+        }else
+            return false;
+    }
+
+    //기능: 메뉴버튼 클릭 시 액티비티 위치 변경 통해 유지
+    protected void onPause() {
+        super.onPause();
+        ActivityManager activityManager = (ActivityManager) getApplicationContext()
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        activityManager.moveTaskToFront(getTaskId(), 0);
     }
 
     @Override
@@ -21,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         //////////////////////////////////////////////////////////////////
         //출처: https://altongmon.tistory.com/395
+        //기능: 안드로이드에서 지원하는 몰입모드 플래그를 True로 설정
         int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
         int newUiOptions = uiOptions;
         boolean isImmersiveModeEnabled = ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
@@ -29,15 +55,12 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Log.i("Is on?", "Turning immersive mode mode on.");
         }
-        // 몰입 모드를 꼭 적용해야 한다면 아래의 3가지 속성을 모두 적용시켜야 합니다
+        // 몰입 모드 적용용 플래그
         newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
         newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
         //////////////////////////////////////////////////////////////////
-
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         super.onCreate(savedInstanceState);
 
@@ -52,14 +75,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        ConstraintLayout cc = new ConstraintLayout(this);
-
-//        Log.d(cc.findViewWithTag("mainMenu"), "asdf");
-//        cc.findViewWithTag("mainMenu").setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Log.d("mainmenu", "asdf");
-//            }
-//        });
+//        ConstraintLayout cc = new ConstraintLayout(this);
     }
 }
